7e1d5e87209c7ad8de8cb1bb9d498db01816de75
prernaumale05 <118253380+prernaumale05@users.noreply.github.com>
2023-01-29 03:18:16 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import Temperature from "./component/Temperature";
+
+const App = () => {
+  return (
+    <div>
+      <Temperature />
+    </div>
+  );
+};
+
+export default App;
